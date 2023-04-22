5585f4cc59a3605f32c2459abc61127661ed817e
jharaghav32 <jharaghav3209@gmail.com>
2023-04-22 13:29:03 +0530

    added viewcontact

--- src/component/Contact.tsx
+++ src/component/Contact.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
 import {useAppSelector,useAppDispatch} from '../hook';
 import {addContact,deleteContact, editContact} from '../feature/ContactSlice.js'
-
+interface totalData{
+    firstname:string,
+    lastname:string,
+    status:string,
+    id : string
+}
 const Contact = () => {
     const dispatch  = useAppDispatch();
     // get redux state using useAppSelector
@@ -11,6 +16,8 @@ const Contact = () => {
     const [singcontact,setsingcontact]=useState({firstname:"",lastname:"",status:""});
     const [showEdit,setshowEdit]=useState(false);
     const [contactlist,setcontactlist]=useState(true);
+    const [viewData,setviewData]=useState<totalData | null>(null);
+    const[view,setview]=useState(false);
     const[editId,seteditId]=useState();
     console.log(data)
     // add new contact 
@@ -22,6 +29,13 @@ const Contact = () => {
             "lastname":contact.lastname,
             "status":contact.status
         }))
+    }
+    const handleView=(id:any)=>{
+        setcontactlist(false);
+        setshowEdit(false);
+        setview(true);
+        const newData = data[id];
+        setviewData(newData);
     }
     //It delete the contact 
     const handleDelete=(id:any)=>{
@@ -50,13 +64,20 @@ const Contact = () => {
         "id":editId
     }));
    }
+   const handleClose=()=>{
+    setcontactlist(true);
+    setshowEdit(false);
+    setshowModal(false);
+    setview(false)
+   }
     const handleChange=(e: React.ChangeEvent<HTMLInputElement>)=>{
         setcontact({...contact,[e.target.name]:e.target.value})
     }
   return (
     <div className='p-2 flex flex-col w-[100%] items-center '>
         <button data-modal-target="defaultModal" data-modal-toggle="defaultModal" className='rounded-none h-10   bg-blue-500 p-1 justify-center' type='button' onClick={()=>{setshowModal(true);
-        setcontactlist(false)}}
+        setcontactlist(false);
+         setshowEdit(false)}}
          >Create Contact</button>
      {showModal ? ( <div id='defaultModal' className='p-5 border-2 border-black m-2'>
         
@@ -85,11 +106,12 @@ const Contact = () => {
 </div>
             </div>
         </form>
-            <div className='flex justify-center items-center'>
+            <div className='flex justify-center items-center gap-2'>
                 <button data-modal-hide="defaultModal" className='bg-blue-500 py-2 px-4 my-2' onClick={handleClick} >Save</button>
+                <button data-modal-hide="defaultModal" className='bg-blue-500 py-2 px-4 my-2' onClick={handleClose} >Close</button>
             </div>
        </div>):null}
-<div className='flex flex-col my-2  gap-1 border overflow-auto  h-[70%] w-[50%]'>
+<div className={`flex flex-col my-2  gap-1 border overflow-auto  h-[70%] w-[50%] ${contactlist || view || showEdit ?'':'hidden'} `}>
    {
      contactlist &&  data.map((e:any,id:any)=>(
         <div className='flex flex-row py-1 px-2 justify-between  gap-7 text-xl my-2 w-[100%] border-2 rounded-2xl border-black '>
@@ -98,7 +120,9 @@ const Contact = () => {
             <button className='bg-green-400   rounded-full px-4 py-2  text-white' onClick={()=>{handleEdit(e.firstname,e.lastname,e.status,e.id)}}  >Edit</button>
             <button className='bg-red-500  rounded-full p-2 text-white' onClick={()=>
                handleDelete(e.id)   
-            } >Delete</button></div>
+            } >Delete</button>
+            <button className='bg-gray-500  rounded-full px-4 py-2 text-white' onClick={()=>handleView(id)} >View</button>
+            </div>
        </div>
        ))}
           {showEdit ? ( <div id='defaultModal' className='p-5 border-2 border-black m-2'>
@@ -128,10 +152,20 @@ const Contact = () => {
 </div>
         </div>
     </form>
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-center items-center gap-2'>
             <button data-modal-hide="defaultModal" className='bg-blue-500 py-2 px-4 my-2' onClick={handleSubmit} >SaveEditedContact</button>
+            <button data-modal-hide="defaultModal" className='bg-blue-500 py-2 px-4 my-2' onClick={handleClose} >Close</button>
         </div>
    </div>):null}
+   {
+    view?<div className='flex p-2 flex-col gap-1 text-xl text-center justify-center items-center shadow-md '>
+        <div className='text-2xl text-center'>Contact Detail</div>
+        <div>FirstName:<span className='text-green-400 mx-4'>{viewData?.firstname}</span></div>
+        <div>LastName:<span className='text-green-400 mx-4'>{viewData?.lastname}</span></div>
+        <div>Status:<span className='text-green-400 mx-4'>{viewData?.status}</span></div>
+        <button onClick={handleClose} className='p-2 bg-gray-400 text-white w-1/2'>Close</button>
+    </div>
+   :null}
     </div>
    </div>
   )
